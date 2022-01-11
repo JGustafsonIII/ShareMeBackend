1d1547d5e46d3722e1b962671f509f6a7d400cef
John Gustafson <John.Gustafson1@Marist.edu>
2022-01-10 20:31:32 -0500

    imported new schemas

--- schemas/schema.js
+++ schemas/schema.js
@@ -5,6 +5,10 @@ import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 import user from './user';
+import pin from './pin';
+import comment from './comment';
+import postedBy from './postedBy';
+import save from './save';
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -15,5 +19,10 @@ export default createSchema({
   types: schemaTypes.concat([
     /* Your types here! */
     user,
+    pin,
+    pin,
+    postedBy,
+    comment,
+    save,
   ]),
 });
